4113db5b655fb1daa1aec540a97af7661f545814
Treigh <treighpugh@gmail.com>
2016-02-19 22:24:50 -0500

    Update setup instructions

--- _build/setup.options.php
+++ _build/setup.options.php
@@ -6,6 +6,6 @@ $output = '<img src="http://cdn.kleverr.com/pjx/quill/img/quill-logo.svg" width=
 <p><strong>Set a name for the default Post Author (Usually the blog\'s admin).</strong></p>
 <input type="text" name="default_author_name" title="Default Author" />
 <br />
-<p>After installation, don\'t forget to turn on <strong>friendly_urls</strong> in System Settings and rename the <i>ht.access</i> file to <b>.htaccess</b>. To quickly get started with Quill, check out <a href="http://quill.kleverr.modxcloud.com/flight-manual" target="_blank">these easy steps</a>.</p>';
+<p>After installation, don\'t forget to turn on <strong>friendly_urls</strong> in System Settings and rename the <i>ht.access</i> file to <b>.htaccess</b>. To quickly get started with Quill, check out <a href="http://quill.kleverr.modxcloud.com/flight-manual" target="_blank">the Flight Manual</a>.</p>';
 
 return $output;
