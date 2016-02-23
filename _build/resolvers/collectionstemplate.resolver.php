0eef96a914bdf1fc9f24655f94c5ece0f5812cff
Treigh <treighpugh@gmail.com>
2016-02-23 07:04:44 -0500

    Shorten variable names

--- _build/resolvers/collectionstemplate.resolver.php
+++ _build/resolvers/collectionstemplate.resolver.php
@@ -23,30 +23,32 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
             )
         );
 
-        $template = $modx->getObject('modTemplate', array('templatename' => 'Quill'));
+        $resTemplate = $modx->getObject('modTemplate', array('templatename' => 'Quill'));
 
         /* Create Collections template */
-        $collectionsTemplate = $modx->getObject('CollectionTemplate', array('name' => 'Quill'));
-        if (!$collectionsTemplate) {
-            $collectionsTemplate = $modx->newObject('CollectionTemplate');
-            $collectionsTemplate->set('name', 'Quill');
-            $collectionsTemplate->set('description', 'A custom view for Quill-themed blogs.');
-            $collectionsTemplate->set('global_template', false);
-            $collectionsTemplate->set('bulk_actions', true);
-            $collectionsTemplate->set('allow_dd', true); // => Allow drag & drop
-            $collectionsTemplate->set('page_size', 10);
-            $collectionsTemplate->set('sort_field', 'publishedon');
-            $collectionsTemplate->set('sort_dir', 'desc');
-            $collectionsTemplate->set('child_template', null);
-            $collectionsTemplate->set('child_resource_type', 'modDocument');
-            $collectionsTemplate->set('resource_type_selection', true);
-            $collectionsTemplate->set('tab_label', 'Posts');
-            $collectionsTemplate->set('button_label', 'New Post');
-            $collectionsTemplate->set('back_to_collection_label', 'All Posts');
+        $ct = $modx->getObject('CollectionTemplate', array('name' => 'Quill'));
 
-            if ($template) {
-                $collectionsTemplate->set('child_template', $template->id);
+        if (!$ct) {
+            $ct = $modx->newObject('CollectionTemplate');
+            $ct->set('name', 'Quill');
+            $ct->set('description', 'A custom view for Quill-themed blogs.');
+            $ct->set('global_template', false);
+            $ct->set('bulk_actions', true);
+            $ct->set('allow_dd', true); // => Allow drag & drop
+            $ct->set('page_size', 10);
+            $ct->set('sort_field', 'publishedon');
+            $ct->set('sort_dir', 'desc');
+            $ct->set('child_template', null);
+            $ct->set('child_resource_type', 'modDocument');
+            $ct->set('resource_type_selection', true);
+            $ct->set('tab_label', 'Posts');
+            $ct->set('button_label', 'New Post');
+            $ct->set('back_to_collection_label', 'All Posts');
+
+            if ($resTemplate) {
+                $ct->set('child_template', $resTemplate->id);
             }
+
             /* Add Collections Columns */
             $columns = array();
             $columns[0] = $modx->newObject('CollectionTemplateColumn');
@@ -109,17 +111,17 @@ switch ($options[xPDOTransport::PACKAGE_ACTION]) {
                 'position' => 4,
             ));
 
-            $collectionsTemplate->addMany($columns, 'Columns');
-            $collectionsTemplate->save();
+            $ct->addMany($columns, 'Columns');
+            $ct->save();
         }
 
         /* Assign Collections template to the blog container */
         $ctObj = $modx->getObject('CollectionTemplate', array('name' => 'Quill'));
         $csTemplate = $ctObj->get('id');
-        $collectionsSettings = $modx->newObject('CollectionSetting');
-        $collectionsSettings->set('collection', $modx->getOption('quill2.blog_container'));
-        $collectionsSettings->set('template', $csTemplate);
-        $collectionsSettings->save();
+        $cs = $modx->newObject('CollectionSetting');
+        $cs->set('collection', $modx->getOption('quill2.blog_container'));
+        $cs->set('template', $csTemplate);
+        $cs->save();
 
         break;
     case xPDOTransport::ACTION_UNINSTALL:
