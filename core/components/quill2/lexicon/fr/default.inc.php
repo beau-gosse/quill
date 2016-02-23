cec3690ca12ccf1049d678ea503cc9c3d59df3b3
Treigh <treighpugh@gmail.com>
2016-02-23 12:40:34 -0500

    Remove unnecessary articles

--- core/components/quill2/lexicon/fr/default.inc.php
+++ core/components/quill2/lexicon/fr/default.inc.php
@@ -8,20 +8,20 @@
 
 // Used in System Settings
 $_lang['setting_quill2.doc_container']            = 'Conteneur global.';
-$_lang['setting_quill2.doc_container_desc']       = 'L\'identifiant du conteneur glabal de Quill. Il est utilisé pour organiser les ressources de Quill lors d\'une nouvelle installation.';
+$_lang['setting_quill2.doc_container_desc']       = 'Identifiant du conteneur glabal de Quill. Il est utilisé pour organiser les ressources de Quill lors d\'une nouvelle installation.';
 $_lang['setting_quill2.blog_container']           = 'Conteneur du Blog.';
-$_lang['setting_quill2.blog_container_desc']      = 'L\'identifiant du conteneur du Blog de Quill.';
+$_lang['setting_quill2.blog_container_desc']      = 'Identifiant du conteneur du Blog de Quill.';
 $_lang['setting_quill2.sections_page']            = 'Le conteneur des sections';
-$_lang['setting_quill2.sections_page_desc']       = 'L\'identifiant de la ressource contenant la liste des sections.';
+$_lang['setting_quill2.sections_page_desc']       = 'Identifiant de la ressource contenant la liste des sections.';
 $_lang['setting_quill2.topics_page']              = 'Page cible des sujets.';
-$_lang['setting_quill2.topics_page_desc']         = 'L\'identifiant de la page cible utilis&eacute; pour g&eacute;n&eacute;rer l\'URI des sujets (tags).';
+$_lang['setting_quill2.topics_page_desc']         = 'Identifiant de la page cible utilis&eacute; pour g&eacute;n&eacute;rer l\'URI des sujets (tags).';
 $_lang['setting_quill2.authors_page']             = 'Conteneur des auteurs.';
-$_lang['setting_quill2.authors_page_desc']        = 'L\'identifiant de la ressource contenant la liste des auteurs.';
+$_lang['setting_quill2.authors_page_desc']        = 'Identifiant de la ressource contenant la liste des auteurs.';
 $_lang['setting_quill2.default_author_page']      = 'Auteur par d&eacute;faut.';
-$_lang['setting_quill2.default_author_page_desc'] = 'L\'identifiant de la ressource de l\'auteur par d&eacute;faut de Quill.';
+$_lang['setting_quill2.default_author_page_desc'] = 'Identifiant de la ressource de l\'auteur par d&eacute;faut de Quill.';
 $_lang['setting_quill2.archives_page']            = 'Page des archives.';
-$_lang['setting_quill2.archives_page_desc']       = 'L\'identifiant de la page des archives.';
+$_lang['setting_quill2.archives_page_desc']       = 'Identifiant de la page des archives.';
 $_lang['setting_quill2.search_page']              = 'Page de recherche.';
-$_lang['setting_quill2.search_page_desc']         = 'L\'identifiant de la page de recherche du Blog.';
+$_lang['setting_quill2.search_page_desc']         = 'Identifiant de la page de recherche du Blog.';
 $_lang['setting_quill2.rss_page']                 = 'Page RSS.';
-$_lang['setting_quill2.rss_page_desc']            = 'L\'identifiant de la page RSS du Blog.';
+$_lang['setting_quill2.rss_page_desc']            = 'Identifiant de la page RSS du Blog.';
